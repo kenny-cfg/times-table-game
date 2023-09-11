bf446604fca5888a5c9d08e9c7780262d7622d4d
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-09-11 19:51:03 +0100

    Generate two random numbers

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('Whoop')
+const firstRandomInteger = getRandomInteger()
+const secondRandomInteger = getRandomInteger()
+
+function getRandomInteger() {
+  return Math.floor(Math.random() * 12) + 1
+}
