5e6496a26a97bc25250f85a6c849512df2e8458d
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-09-11 20:19:09 +0100

    Countdown

--- script.js
+++ script.js
@@ -4,12 +4,23 @@ const submitButton = document.getElementById('submitAnswer')
 const inputBox = document.getElementById('answerBox')
 const answerResponseHolder = document.getElementById('answerResponse')
 const scoreContainer = document.getElementById('scoreContainer')
+const countdownContainer = document.getElementById('countdownContainer')
 let correctAnswers = 0
 let incorrectAnswers = 0
 
 let firstRandomInteger
 let secondRandomInteger
+let countdown
 refreshQuestion()
+
+setInterval(() => {
+  countdown--
+  refreshCountdownDisplay()
+}, 1000)
+
+function refreshCountdownDisplay() {
+  countdownContainer.innerText = `You have ${countdown} seconds to answer`
+}
 
 function getRandomInteger() {
   return Math.floor(Math.random() * 12) + 1
@@ -19,9 +30,11 @@ function refreshQuestion() {
   firstRandomInteger = getRandomInteger()
   secondRandomInteger = getRandomInteger()
   inputBox.value = ''
+  countdown = 10
   firstNumberContainer.innerText = firstRandomInteger
   secondNumberContainer.innerText = secondRandomInteger
   scoreContainer.innerText = `You've got ${correctAnswers} correct and ${incorrectAnswers} incorrect`
+  refreshCountdownDisplay()
 }
 
 submitButton.onclick = () => {
