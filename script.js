b8666ca130a133d7b823454a2832512b1edee5ab
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-09-11 20:28:03 +0100

    Store localstorage scores and retrieve

--- script.js
+++ script.js
@@ -5,8 +5,14 @@ const inputBox = document.getElementById('answerBox')
 const answerResponseHolder = document.getElementById('answerResponse')
 const scoreContainer = document.getElementById('scoreContainer')
 const countdownContainer = document.getElementById('countdownContainer')
-let correctAnswers = 0
-let incorrectAnswers = 0
+let correctAnswers = parseInt(localStorage.getItem('correctAnswers'))
+if (isNaN(correctAnswers)) {
+  correctAnswers = 0
+}
+let incorrectAnswers = parseInt(localStorage.getItem('incorrectAnswers')) 
+if (isNaN(incorrectAnswers)) {
+  incorrectAnswers = 0
+}
 
 let firstRandomInteger
 let secondRandomInteger
@@ -17,11 +23,21 @@ setInterval(() => {
   countdown--
   if (countdown <= 0) {
     answerResponseHolder.innerText = 'OUT OF TIME'
-    incorrectAnswers++
+    answerIsIncorrect()
     refreshQuestion()
   }
   refreshCountdownDisplay()
 }, 1000)
+
+function answerIsCorrect() {
+  correctAnswers++
+  localStorage.setItem('correctAnswers', correctAnswers)
+}
+
+function answerIsIncorrect() {
+  incorrectAnswers++
+  localStorage.setItem('incorrectAnswers', incorrectAnswers)
+}
 
 function refreshCountdownDisplay() {
   countdownContainer.innerText = `You have ${countdown} seconds to answer`
@@ -47,10 +63,10 @@ submitButton.onclick = () => {
   const proposedAnswerAsInteger = parseInt(proposedAnswer)
   // TODO: Deal with NaN
   if (proposedAnswerAsInteger === firstRandomInteger * secondRandomInteger) {
-    correctAnswers++
+    answerIsCorrect()
     answerResponseHolder.innerText = `Yes, ${firstRandomInteger} x ${secondRandomInteger} is indeed ${proposedAnswerAsInteger}`
   } else {
-    incorrectAnswers++
+    answerIsIncorrect()
     answerResponseHolder.innerText = `No, ${firstRandomInteger} x ${secondRandomInteger} is not ${proposedAnswerAsInteger}, it's ${firstRandomInteger * secondRandomInteger}`
   }
   refreshQuestion()
