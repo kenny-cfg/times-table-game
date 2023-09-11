acfbab11c787cb5993b572804f31424169fe72e3
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-09-11 20:03:05 +0100

    Click handler for the submit button

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
+const firstNumberContainer = document.getElementById('firstNumber')
+const secondNumberContainer = document.getElementById('secondNumber')
+const submitButton = document.getElementById('submitAnswer')
+const inputBox = document.getElementById('answerBox')
+
 const firstRandomInteger = getRandomInteger()
 const secondRandomInteger = getRandomInteger()
 
+firstNumberContainer.innerText = firstRandomInteger
+secondNumberContainer.innerText = secondRandomInteger
+
 function getRandomInteger() {
   return Math.floor(Math.random() * 12) + 1
+}
+
+submitButton.onclick = () => {
+  console.log('SUBMITTED')
 }
