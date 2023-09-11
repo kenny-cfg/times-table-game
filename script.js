9e666e920eb09f489b3ecf0d44d16ad5dbf5a4a5
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-09-11 20:15:16 +0100

    Keep score

--- script.js
+++ script.js
@@ -3,15 +3,25 @@ const secondNumberContainer = document.getElementById('secondNumber')
 const submitButton = document.getElementById('submitAnswer')
 const inputBox = document.getElementById('answerBox')
 const answerResponseHolder = document.getElementById('answerResponse')
+const scoreContainer = document.getElementById('scoreContainer')
+let correctAnswers = 0
+let incorrectAnswers = 0
 
-const firstRandomInteger = getRandomInteger()
-const secondRandomInteger = getRandomInteger()
-
-firstNumberContainer.innerText = firstRandomInteger
-secondNumberContainer.innerText = secondRandomInteger
+let firstRandomInteger
+let secondRandomInteger
+refreshQuestion()
 
 function getRandomInteger() {
   return Math.floor(Math.random() * 12) + 1
+}
+
+function refreshQuestion() {
+  firstRandomInteger = getRandomInteger()
+  secondRandomInteger = getRandomInteger()
+  inputBox.value = ''
+  firstNumberContainer.innerText = firstRandomInteger
+  secondNumberContainer.innerText = secondRandomInteger
+  scoreContainer.innerText = `You've got ${correctAnswers} correct and ${incorrectAnswers} incorrect`
 }
 
 submitButton.onclick = () => {
@@ -19,8 +29,11 @@ submitButton.onclick = () => {
   const proposedAnswerAsInteger = parseInt(proposedAnswer)
   // TODO: Deal with NaN
   if (proposedAnswerAsInteger === firstRandomInteger * secondRandomInteger) {
+    correctAnswers++
     answerResponseHolder.innerText = `Yes, ${firstRandomInteger} x ${secondRandomInteger} is indeed ${proposedAnswerAsInteger}`
   } else {
+    incorrectAnswers++
     answerResponseHolder.innerText = `No, ${firstRandomInteger} x ${secondRandomInteger} is not ${proposedAnswerAsInteger}, it's ${firstRandomInteger * secondRandomInteger}`
   }
+  refreshQuestion()
 }
